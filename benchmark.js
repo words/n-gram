72b1b7fe20f32c600607d2067a8732e1534df98e
wooorm <tituswormer@gmail.com>
2014-11-04 16:50:03 +0100

    Refactor benchmark

--- benchmark.js
+++ benchmark.js
@@ -12,23 +12,23 @@ nGram = require('./');
  * Optional dependencies.
  */
 
-var exception,
-    ngram,
-    madbenceNgram;
+var ngram,
+    madbenceNgram,
+    hasException;
 
 try {
     madbenceNgram = require('madbence-ngram');
 } catch (err) {
-    exception = err;
+    hasException = true;
 }
 
 try {
     ngram = require('ngram').ngram;
 } catch (err) {
-    exception = err;
+    hasException = true;
 }
 
-if (exception) {
+if (hasException) {
     console.log(
         '\u001B[0;31m' +
         'The libraries needed by this benchmark could not be found. ' +
@@ -80,54 +80,58 @@ suite('nGram -- this module', function () {
     });
 });
 
-suite('madbence/ngram', function () {
-    bench('  bigrams on a sentence', function () {
-        madbenceNgram(sentence, 2, 2);
-    });
+if (madbenceNgram) {
+    suite('madbence/ngram', function () {
+        bench('  bigrams on a sentence', function () {
+            madbenceNgram(sentence, 2, 2);
+        });
 
-    bench('  bigrams on an article', function () {
-        madbenceNgram(article, 2, 2);
-    });
+        bench('  bigrams on an article', function () {
+            madbenceNgram(article, 2, 2);
+        });
 
-    bench(' trigrams on a sentence', function () {
-        madbenceNgram(sentence, 3, 3);
-    });
+        bench(' trigrams on a sentence', function () {
+            madbenceNgram(sentence, 3, 3);
+        });
 
-    bench(' trigrams on an article', function () {
-        madbenceNgram(article, 3, 3);
-    });
+        bench(' trigrams on an article', function () {
+            madbenceNgram(article, 3, 3);
+        });
 
-    bench('ten-grams on a sentence', function () {
-        madbenceNgram(sentence, 10, 10);
-    });
+        bench('ten-grams on a sentence', function () {
+            madbenceNgram(sentence, 10, 10);
+        });
 
-    bench('ten-grams on an article', function () {
-        madbenceNgram(article, 10, 10);
+        bench('ten-grams on an article', function () {
+            madbenceNgram(article, 10, 10);
+        });
     });
-});
+}
 
-suite('ngram', function () {
-    bench('  bigrams on a sentence', function () {
-        ngram(sentence, 2, 2);
-    });
+if (ngram) {
+    suite('ngram', function () {
+        bench('  bigrams on a sentence', function () {
+            ngram(sentence, 2, 2);
+        });
 
-    bench('  bigrams on an article', function () {
-        ngram(article, 2, 2);
-    });
+        bench('  bigrams on an article', function () {
+            ngram(article, 2, 2);
+        });
 
-    bench(' trigrams on a sentence', function () {
-        ngram(sentence, 3, 3);
-    });
+        bench(' trigrams on a sentence', function () {
+            ngram(sentence, 3, 3);
+        });
 
-    bench(' trigrams on an article', function () {
-        ngram(article, 3, 3);
-    });
+        bench(' trigrams on an article', function () {
+            ngram(article, 3, 3);
+        });
 
-    bench('ten-grams on a sentence', function () {
-        ngram(sentence, 10, 10);
-    });
+        bench('ten-grams on a sentence', function () {
+            ngram(sentence, 10, 10);
+        });
 
-    bench('ten-grams on an article', function () {
-        ngram(article, 10, 10);
+        bench('ten-grams on an article', function () {
+            ngram(article, 10, 10);
+        });
     });
-});
+}
